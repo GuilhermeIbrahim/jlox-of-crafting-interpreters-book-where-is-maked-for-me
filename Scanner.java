e703b5c1a3184468f9059e21a65b0d8ee160755c
GuilhermeIbrahim <guilhermeibrahim08@gmail.com>
2023-03-16 10:05:13 -0300

    Added the scan of Longer Lexemes to the Scanner

--- Scanner.java
+++ Scanner.java
@@ -52,6 +52,23 @@ class Scanner {
             case '>':
                 addToken(match('=') ? GREATER_EQUAL : GREATER);
                 break;
+            case '/':
+                if (match('/')) {
+                    // A comment goes until the end of the line.
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+
+                case ' ':
+                case '\r':
+                case '\t':
+                    // Ignore whitespaces
+                    break;
+                case '\n':
+                    line++;
+                    break;
 
             default:
                 Lox.error(line, "Unexpected character.");
@@ -65,6 +82,11 @@ class Scanner {
 
         current++;
         return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
     }
 
     private boolean isAtEnd() {
